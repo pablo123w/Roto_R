bdf3c04b684dc2a3da3a80880009dec0bd66b892
Jon <jonmills3000@gmail.com>
2022-09-02 22:31:50 -0700

    Objects that blow up / crumble
    Added Script
    Models

---
+++ Roto_Res/Assets/Scripts/breakObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * ATTACH THIS SCRIPT TO THE OBJECT THAT IS BEING DESTROYED
+ */
+
+public class breakObject : MonoBehaviour
+{
+    // Spawns destroyed prefab
+    public Transform destroyedObject;
+    // Strength of object
+    public float objectStrength;
+    // The distance that the force effects the object
+    public float objectExplosionRadius;
+    // The distance that the "explosion" sends the pieces
+    public float explosionPower;
+    // The force that sends the broken pieces flying up
+    public float upwardsExplosion;
+
+    // When object hits the prefab hard enough destroys the object.
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Anything that hits the prefab with greater force than the strength of the object is destroyed.
+        if (collision.relativeVelocity.magnitude > objectStrength)
+        {
+            Destroy(gameObject);
+            Instantiate(destroyedObject, transform.position, transform.rotation);
+            destroyedObject.localScale = transform.localScale;
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere (explosionPos, objectExplosionRadius);
+
+            foreach (Collider hit in colliders)
+            {
+                if (hit.attachedRigidbody)
+                {
+                    hit.attachedRigidbody.AddExplosionForce(explosionPower * collision.relativeVelocity.magnitude, explosionPos, objectExplosionRadius, upwardsExplosion);
+                }
+            }
+        }
+    }
+}
