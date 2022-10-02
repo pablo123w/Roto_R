6481326da6c2dcaca635cf66ec04e139f0bbbf73
Jon <jonmills3000@gmail.com>
2022-10-01 23:04:55 -0700

    WIP + Mingun

--- Roto_Res/Assets/Scripts/LevelProgression.cs
+++ Roto_Res/Assets/Scripts/LevelProgression.cs
@@ -7,6 +7,7 @@ public class LevelProgression : MonoBehaviour
 {
     public float gooberConstraint = 1;
     public float gooberSaved = 0;
+    //public int totalGoobers;
 
     public Image GooberConstraintImage;
     public Image GooberSavedImage;
@@ -16,7 +17,10 @@ public class LevelProgression : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Tracks the amount of the goobers in the scene.
+        GameObject[]totalGoobers = GameObject.FindGameObjectsWithTag("Goober");
+        int gooberCount = totalGoobers.Length;
+        Debug.Log(gooberCount + " Goober(s) are in the scene.");
     }
 
     // Update is called once per frame
@@ -37,15 +41,36 @@ public class LevelProgression : MonoBehaviour
             gooberConstraint -= 0.1f;
             GooberConstraintImage.fillAmount = gooberConstraint;
         }
-
+        // If 50% or more goobers have been saved then the next level button is revealed.
         if (gooberSaved >= 0.5f)
         {
             ButtonActive();
         }
-    }
 
+        ///////////////////////////////////////////////////////Testing with GOOBERS//////////////////////////////////////////////////////
+
+        
+        
+
+        /*if (gooberSaved >= totalGoobers)
+        {
+            ButtonActive();
+        }
+        */
+    }
+    
+    // Selecting this button changes the current scene to the next one.
     public void ButtonActive()
     {
             NextLevelButton.SetActive(true);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Goober")
+        {
+            gooberSaved += 0.1f;
+            Debug.Log("A Goober was hit!");
+        }
     }
 }
